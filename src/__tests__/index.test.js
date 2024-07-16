2400897de9076e1a38476949b649a746e7b29309
Nas2 <nastasyata2r@gmail.com>
2024-07-16 10:26:03 +0200

    bugs in import and operation of the function have been fixed

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,21 +1,21 @@
-import { getPercents } from "../index.js";
+const getPercents = require("../index.js");
 
 describe("negative tests for getPercents function", () => {
   it("Enter a negative number as a percentage.", () => expect(getPercents(-10,200)).toBe("В качестве процента передано некорректное число"));
 });
 describe("positive tests for getPercents function", () => {
-    it("Entered a valid (positive) number as a percentage.", () => expect(getPercents(50,89)).toBe("44.5"));
+    it("Entered a valid (positive) number as a percentage.", () => expect(getPercents(50,89)).toBe('44.5'));
 });
 describe("positive tests for getPercents function", () => {
-    it("Percentage of a negative number.", () => expect(getPercents(25,-100)).toBe("-25"));
+    it("Percentage of a negative number.", () => expect(getPercents(25,-100)).toBe('-25'));
 });
 describe("positive tests for getPercents function", () => {
-    it("Getting a percentage greater than 100%.", () => expect(getPercents(300,444)).toBe("1332"));
+    it("Getting a percentage greater than 100%.", () => expect(getPercents(300,444)).toBe('1332'));
 });
 describe("positive tests for getPercents function", () => {
-    it("Getting a fractional percentage of a number.", () => expect(getPercents(50.5,1010)).toBe("510.05"));
+    it("Getting a fractional percentage of a number.", () => expect(getPercents(50.5,1010)).toBe('510.05'));
 });
 describe("Limit values for getPercents function", () => {
-    it("Values 0 percent", () => expect(getPercents(0,333)).toBe("0"));
-    it("Values 100 percent", () => expect(getPercents(100,54)).toBe("54"));
+    it("Values 0 percent", () => expect(getPercents(0,333)).toBe('0'));
+    it("Values 100 percent", () => expect(getPercents(100,54)).toBe('54'));
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 //Напишите функцию getPercents(percent, number), которая возвращает {percent} процентов от {number}.
-export function getPercents (percent, number){
+function getPercents (percent, number){
+  let resalt;
   if (percent >= 0) {
-    const resalt = percent/100*number;
-    //console.log(resalt);
+    resalt = `${percent/100*number}`;
   }
   else {
     resalt = 'В качестве процента передано некорректное число';
   }
   return resalt;
 }
+module.exports = getPercents;
